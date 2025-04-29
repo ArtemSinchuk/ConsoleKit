3e6d208aee7112a3e61d105c4275f66156392c0d
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-04-29 13:27:17 +0200

    Update Encoding.java

--- ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
+++ ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
@@ -5,4 +5,5 @@ public class Encoding {
     public static final String STRIKETHROUGH = "\u001B[9m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
+    public static final String BLUE = "\033[0;34m";
 }
