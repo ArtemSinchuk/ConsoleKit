61a1376e5267e67954b7829aa6fab8de76f20507
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-04-30 12:46:21 +0200

    added new color

--- ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
+++ ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
@@ -5,5 +5,6 @@ public class Encoding {
     public static final String STRIKETHROUGH = "\u001B[9m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
-    public static final String BLUE = "\033[0;34m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
 }
