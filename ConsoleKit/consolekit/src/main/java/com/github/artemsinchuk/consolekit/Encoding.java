1077d487c1df235a16f16928a55f1824eda0138e
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-05-03 16:43:28 +0200

    added magenta

--- ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
+++ ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
@@ -7,4 +7,6 @@ public class Encoding {
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
     public static final String BLUE = "\u001B[34m";
+    public static final String MAGENTA = "\u001B[35m";
+    
 }
