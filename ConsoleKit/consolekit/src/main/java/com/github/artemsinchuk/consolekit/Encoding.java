80a86dc705d9ce3b97f886456e341d81c9d20817
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-05-17 18:29:36 +0200

    Update Encoding.java

--- ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
+++ ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
@@ -6,6 +6,7 @@ public class Encoding {
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
+    public static final String BRIGHT_BLUE = "\u001B[34;1m";
     public static final String BLUE = "\u001B[34m";
     public static final String MAGENTA = "\u001B[35m";
     public static final String CYAN = "\u001B[36m";
