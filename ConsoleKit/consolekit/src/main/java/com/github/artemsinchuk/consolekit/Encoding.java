84989a674546d0c6a1cb4b9c74b97976d517620b
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-06-17 12:13:09 +0200

    Update Encoding.java

--- ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
+++ ConsoleKit/consolekit/src/main/java/com/github/artemsinchuk/consolekit/Encoding.java
@@ -33,4 +33,5 @@ public class Encoding {
 
     public static final String BRIGHT_BACKGROUND_BLACK = "\u001B[40;1m";
     public static final String BRIGHT_BACKGROUND_RED = "\u001B[41;1m";
+    public static final String BRIGHT_BACKGROUND_GREEN = "\u001B[42;1m";
 }
